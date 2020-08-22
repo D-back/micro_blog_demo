347f64eb8d4ebba34bda63d204a66113b00c6660
D-back <634008415@qq.com>
2020-08-22 15:20:15 +0800

    完善了注册功能，添加了登录功能

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from flask import  Blueprint
 from flask import request
 from flask import session
+from flask import redirect
 from flask import render_template
 
 from user.model import User
@@ -11,6 +12,7 @@ user_dp = Blueprint('user',__name__,url_prefix='/user')
 user_dp.template_folder = './templates'
 
 
+#注册页面
 @user_dp.route('/register',methods=('POST','GET'))
 def register():
     if request.method == 'POST':
@@ -20,13 +22,40 @@ def register():
         age = request.form.get('age')
         city = request.form.get('city')
         phone = request.form.get('phone')
+        try:
+            user = User.query.filter_by( username = username ).one()
+            if user :
+                return '用户名已存在'
+        except Exception:
+            user = User(username=username,password=password,gender=gender,
+                        age=age,city=city,phone=phone)
+            db.session.add(user)
+            db.session.commit()
 
-        user = User(username=username,password=password,gender=gender,
-                    age=age,city=city,phone=phone)
-        db.session.add(user)
-        db.session.commit()
-        return render_template('/user/login')
+        return redirect('/user/login')
 
 
     else:
         return render_template('register.html')
+
+#登录页面
+@user_dp.route('/login',methods=('POST','GET'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username = username).one()
+        if not user :
+            return '用户名不存在'
+        if password != user.password:
+            return '密码错误，请重新输入'
+        session['user'] = user.id
+
+        return redirect('/user/info')
+
+
+    else:
+        return render_template('login.html')
+
+
